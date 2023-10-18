34ce204b79698184f73fbe18b5c0a75371931edc
pareramelisa <pareralucenamelisa@gmail.com>
2023-10-18 20:00:07 -0300

    presave-changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,20 @@
 import isoLogo from './assets/logo.svg'
 import connectifyLogo from '/connectify.svg'
 import './App.css'
+import NavBar from './components/register/NavBar/NavBar'
 
 function App() {
  
 
   return (
     <>
-      <div>
+      <NavBar/>
+      {/* <div>
         <a href="https://github.com/GiseleCuello/Connectify/tree/main" target="_blank">
           <img src={isoLogo} className="logo-spiner" alt="iso logo Connectify" />
         </a>
       </div>
-      <img src={connectifyLogo} className="logo" alt="Connectify logo" />
+      <img src={connectifyLogo} className="logo" alt="Connectify logo" /> */}
 
     </>
   )
