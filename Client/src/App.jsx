f7a783c0b3f27bf5be48314c83f944c53d1fe1be
pareramelisa <pareralucenamelisa@gmail.com>
2023-10-18 20:02:41 -0300

    fix config

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
 import isoLogo from './assets/logo.svg'
 import connectifyLogo from '/connectify.svg'
 import './App.css'
+
 import NavBar from './components/register/NavBar/NavBar'
 
 
@@ -15,13 +16,7 @@ function App() {
           <img src={isoLogo} className="logo-spiner" alt="iso logo Connectify" />
         </a>
       </div>
-<<<<<<< HEAD
       <img src={connectifyLogo} className="logo" alt="Connectify logo" /> */}
-=======
-      <img src={connectifyLogo} className="logo" alt="Connectify logo" />
-      <ButtonUsage/>
->>>>>>> 6308bef3942ae0005c8dab1658a80163e6bd4961
-
     </>
     
   )
