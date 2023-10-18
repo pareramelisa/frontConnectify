716fdfd8254ae7674e7a0b64088b7c9331bcb8f7
Lucas Fraggioli <fraggioli.lucas@gmail.com>
2023-10-18 12:56:06 -0300

    creados store y action de profesionales

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import { ProfTest } from "./components/ProfTest";
 
 function App() {
-
   return (
     <>
       <div>
         <h1>Front Connectify</h1>
+        <ProfTest />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
