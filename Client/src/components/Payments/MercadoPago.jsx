b8744265b0c601c3bbec4a5478d8f6c967192f2a
Andy Ojeda <tuar_a@hotmail.com>
2023-10-20 14:19:04 -0300

    Componente MercadoPago agregado

---
+++ Client/src/components/Payments/MercadoPago.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+
+
+function mercadoPago({price, description}) {
+
+    const [preferenceId, setPreferenceId] = useState(null);
+    const [descriptionBuy, setDescriptionBuy] = useState("Gracias por la compra!")
+    const [servicePrice, setServicePrice] = useState(1);
+    const [cargandoSiNo, setCargandoSiNo] = useState("");
+    const [walletVisible, setWalletVisible] = useState(false);
+
+    useEffect(() => {
+        if (preferenceId) {
+          setCargandoSiNo("");
+          setWalletVisible(true);   //Evito que se repita el botón repetido de MP
+        }
+      }, [preferenceId]);
+    
+      useEffect(() => {
+        if (price) setServicePrice(price);
+        if (description) setDescriptionBuy(description);
+      }, [price, description]);
+     
+    
+
+    //!Cambiar Public Key de MercadoPago usando .env...
+    initMercadoPago("TEST-50156f30-252b-4623-bbba-ed453620d49f");
+
+    //Mercado Pago functions
+    const createPreference = async ()=>{
+        
+        try {
+            const response = await axios.post("http://localhost:3001/create_preference", 
+            {
+                description: descriptionBuy,
+                price:servicePrice,
+                quantity: 1,
+            })
+
+            const { id } = response.data;
+            return id;
+
+
+        } catch (error) {
+            console.log("Error en solicitud MercadoPago... ",error);
+        }
+    }
+
+
+    const handleButton = async ()=>{
+        setCargandoSiNo("Cargando compra...");
+        const id = await createPreference();
+        if (id) {
+            setPreferenceId(id);
+        }
+    };
+
+
+    const handleChange = (event) =>{
+        const value = event.target.value;
+        setServicePrice(value);
+    }
+
+    
+
+  return (
+    <>
+        <p>Comprar...</p>
+        <div className='contentPrice'>
+            <label className='price$'>$</label>
+            <input type='text' className='priceValue' value={servicePrice} onChange={handleChange}/>
+        </div>
+
+        <button className='donate-link' onClick={handleButton}>COMPRAR</button>
+        <p>{cargandoSiNo}</p>
+
+        {   
+            
+            walletVisible && preferenceId && <Wallet initialization={{ preferenceId }}/>
+            
+        }
+
+
+    </>
+  )
+}
+
+export default mercadoPago;
