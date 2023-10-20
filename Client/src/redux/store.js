f6d0d3d1219f6088b2e6dfb30157966460d6a5cc
Daniela Dell Acqua <ddacqua@gmail.com>
2023-10-20 19:00:14 -0300

    Ruteo completo  permitidas estado en redux userRol

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adsSlice from "./actions/adsSlice";
 import professionalSlice from "./actions/professionalSlice";
+import userSlice from "./actions/userSlice";
 
 export default configureStore({
   reducer: {
     ads: adsSlice,
     professionals: professionalSlice,
+    user: userSlice,
   },
 });
--- Client/src/views/Landing/Landing.jsx
+++ Client/src/views/Landing/Landing.jsx
@@ -1,18 +1,56 @@
-import { NavLink } from "react-router-dom";
-import React from "react";
-import "./Landing.css";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Landing.css';
+import { useDispatch } from 'react-redux';
+import { setUserRole } from '../../redux/actions/userSlice';
 
 const Landing = () => {
-    return (
-        <>
-        <div className="landing-container"> 
-       
-        <NavLink  to={'/home'}>
-                    <button className="entrar">ENTRAR</button>
-                </NavLink>
-        </div>
-        </>
-    )
-}
+  const dispatch = useDispatch(); // Accede a la función dispatch para llamar a setUserRole
+
+  const handleRoleChange = (role) => {
+    dispatch(setUserRole(role));
+  };
+
+  return (
+    <div className="landing-container">
+      <NavLink to="/home">
+        <button className="entrar" onClick={() => handleRoleChange('')}>
+          Iniciar mi búsqueda (SearchBar)
+        </button>
+      </NavLink>
+      <div>
+        <NavLink to="/client/login">
+          <button className="entrar" onClick={() => handleRoleChange('client')}>
+            Ya soy cliente
+          </button>
+        </NavLink>
+
+        <NavLink to="/client/registration">
+          <button className="entrar">Soy nuevo Cliente</button>
+        </NavLink>
+      </div>
+
+      <div>
+        <NavLink to="/professional/login">
+          <button className="entrar" onClick={() => handleRoleChange('expert')}>
+            Ya soy Profesional
+          </button>
+        </NavLink>
+
+        <NavLink to="/professional/registration">
+          <button className="entrar" onClick={() => handleRoleChange('expert')}>
+            Soy nuevo Profesional
+          </button>
+        </NavLink>
+      </div>
+
+      <NavLink to="/admin/login">
+        <button className="entrar" onClick={() => handleRoleChange('admin')}>
+          Login de Administrador
+        </button>
+      </NavLink>
+    </div>
+  );
+};
 
 export default Landing;
