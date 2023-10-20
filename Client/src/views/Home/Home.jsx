3d771d27d6a48c7050b5955b4bc50a273df83f81
pareramelisa <pareralucenamelisa@gmail.com>
2023-10-20 14:07:57 -0300

    features/card-professional

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import CardProfessional from '../../components/Card/Card';
+
 
 const Home = () => {
-
 
     return (
         <>
         <h1>vista home</h1>
+        <CardProfessional/>
         </>
     )
 }
