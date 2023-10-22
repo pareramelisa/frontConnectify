18d5cc87fb24ac59394d637efa5f4ca82db20c9a
Daniela Dell Acqua <ddacqua@gmail.com>
2023-10-22 17:08:31 -0300

    actualizacion de css landing, paleta y logo nuevo

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -4,6 +4,7 @@ import './Landing.css';
 import Logo from '../../assets/connectify.svg'
 import Logo2 from '../../assets/logo.svg'
 import SearchBar from './Utils/Searchbar';
+import HomeIcon from '@mui/icons-material/Home'
 
 function LandingPage() {
   
@@ -16,33 +17,30 @@ function LandingPage() {
         <img src={Logo2} alt='Logo' className='logo2'/>
         <img src={Logo} alt='Logo' className='logo'/>
       </div>
-      <Typography variant="h3">
+      <Typography variant="h5" color="#545454">
       Encuentra a los mejores profesionales para tus necesidades.
       </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={()=> {navigate("/login")}}
-        className='button'
-        >
-        Ir al Login
-      </Button>
+      <div className="goHome" >
       <Button
         variant="outlined"
         color="secondary"
         onClick={()=> {navigate("/home")}}
         className='button'
         >
-        Ir a la Página Principal
+         <HomeIcon />
       </Button>
+      </div>
       <SearchBar/>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={()=> {navigate("/login")}}
+        className='button'
+        >
+        Accede a tu cuenta
+      </Button>
       </div>
-      <div className='Card'>
-      <img src='https://thegenuineleather.com/wp-content/uploads/2021/09/Money-Heist-El-Profesor-Black-Blazer.jpg' alt='Profe'></img>
-      <span>Ely</span>
-      <span>Profe de Ingles</span>
-      <span>5.0</span>
-      </div>
+      
     </div>
   );
 }
