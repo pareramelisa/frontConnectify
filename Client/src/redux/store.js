f7f4974e9b22b56469b1e8403774bcbf8a8ef0ba
Andy Ojeda <tuar_a@hotmail.com>
2023-10-23 14:48:51 -0300

    new, del, edit Para crear SearchBar y LocalSelector

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -3,6 +3,7 @@ import adsSlice from "./Slices/adsSlice";
 import professionalSlice from "./Slices/professionalSlice";
 import userLoginSlice from "./Slices/loginSlice";
 import userRegisterSlice from "./Slices/loginSlice";
+import searchReducer from './Slices/searchSlice';
 
 export default configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export default configureStore({
     professionals: professionalSlice,
     usersLogin: userLoginSlice,
     usersRegister: userRegisterSlice,
+    search:searchReducer,
   },
 });
