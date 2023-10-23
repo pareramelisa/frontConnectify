6a89ed808d219e602887b39a72d8a3f5dc478fcb
giselecuello <giselecuello@gmail.com>
2023-10-23 13:56:45 -0300

    cambio carpeta ads

--- Client/src/components/ProfessionalCard/ProfessionalCard.jsx
+++ Client/src/components/ProfessionalCard/ProfessionalCard.jsx
@@ -9,11 +9,11 @@ function Ads() {
   console.log(Ads)
   const dispatch = useDispatch();
   const ads = useSelector(selectAds);
-  console.log(Ads)
+  
   useEffect(() => {
     dispatch(fetchAds());
   }, [dispatch]);
-  console.log(Ads)
+
   return (
     <div className={style.card}>
       {ads.map((ad) => (
