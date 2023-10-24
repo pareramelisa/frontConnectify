fcb5190f2983097f2bd1a81fc8062e4aefa9416c
Drexleer <drexleer01@gmail.com>
2023-10-24 12:43:02 -0500

    Fixed bugs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/dashboardProf" element={<DashboardProf/>} />
+          {/* <Route path="/dashboardProf" element={<DashboardProf/>} /> */}
           <Route path="/detail/:id" element={<Detail />} />
           <Route path="/login" element={<Login />} />
           <Route path="/createAds" element={<CreateAdForm />} />
--- Client/src/redux/Slices/createAdsSlice.js
+++ Client/src/redux/Slices/createAdsSlice.js
@@ -1,36 +1,32 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const createAd = createAsyncThunk('ads/createAd', async (adData) => {
-    try {
-      const endpoint = "https://connectifyback-dp-production.up.railway.app/ads";
-      const response = await axios.post(endpoint, adData);
-      return response.data;
-    } catch (error) {
-      throw error
-    }
-  });
+  const endpoint = 'https://connectifyback-dp-production.up.railway.app/ads';
+  const response = await axios.post(endpoint, adData);
+  return response.data;
+  //eliminado el tryCatch debido a que el createAsyncThunk ya maneja los errores.
+});
 
-  const createAdsSlice = createSlice({
-    name: 'createAds',
-    initialState: { createAds: [], status: "idle", error: null },
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase(createAd.pending, (state) => {
-          state.status = 'loading';
-        })
-        .addCase(createAd.fulfilled, (state, action) => {
-          state.status = 'succeeded';
-          state.createAds.push = action.payload;
-        })
-        .addCase(createAd.rejected, (state, action) => {
-          state.status = 'failed';
-          state.error = action.error.message;
-        });
-    },
-  });
-  
-  export const dataAds = (state) => state.createAds.ads;
-  export default createAdsSlice.reducer;
+const createAdsSlice = createSlice({
+  name: 'createAds',
+  initialState: { createAds: [], status: 'idle', error: null },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createAd.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(createAd.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.createAds.push = action.payload;
+      })
+      .addCase(createAd.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const dataAds = (state) => state.createAds.ads;
+export default createAdsSlice.reducer;
