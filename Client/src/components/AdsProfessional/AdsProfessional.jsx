8f3eafb1c143e771ba80fb460ed45dd482f85e92
pareramelisa <pareralucenamelisa@gmail.com>
2023-10-25 13:50:08 -0300

    maquetado dash prof terminado

---
+++ Client/src/components/AdsProfessional/AdsProfessional.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import HideSourceIcon from '@mui/icons-material/HideSource';
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import { ListSubheader } from '@mui/material';
+
+function AdsProfesional() {
+    const listItemStyle = { fontSize: '20px' }
+  return (
+    <List
+      sx={{
+        width: "600px",
+        bgcolor: "lightGrey",
+        fontSize: "30px",
+        boxShadow: 20,
+        padding: "30px",
+        height: "auto",
+        marginTop: "20px",
+      }}
+    >
+      <ListSubheader sx={{ fontSize: "30px", color: "black" }}>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          Mis anuncios
+          <Link to="/professional/dashboardProf/createAds" style={{ marginLeft: "auto" }}>
+            <IconButton aria-label="create">
+              <AddIcon />
+            </IconButton>
+          </Link>
+        </span>
+      </ListSubheader>
+      {[1, 2, 3].map((value) => (
+        <ListItem
+          key={value}
+          disableGutters
+          secondaryAction={
+            <>
+              <IconButton aria-label="comment">
+                <HideSourceIcon />
+              </IconButton>
+              <IconButton aria-label="edit">
+                <EditIcon />
+              </IconButton>
+            </>
+          }
+          sx={listItemStyle}
+        >
+          <ListItemText
+            primary={`Nombre del anuncio ${value}`}
+            sx={listItemStyle}
+          />
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+export default AdsProfesional
