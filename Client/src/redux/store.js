4fe84e38dd69470e5e340c7f83479c7329822b4c
GGuenov <german.guenov@gmail.com>
2023-10-27 21:57:58 +0100

    Delete/Restore functional + 'sorted'

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -4,7 +4,7 @@ import storage from "redux-persist/lib/storage";
 import adsSlice from "./Slices/adsSlice";
 import professionalSlice from "./Slices/professionalSlice";
 import userLoginSlice from "./Slices/loginSlice";
-import userRegisterSlice from "./Slices/loginSlice";
+import userRegisterSlice from "./Slices/registerSlice";
 import createAdsSlice from "./Slices/createAdsSlice";
 import searchReducer from "./Slices/searchSlice";
 import detailSlice from "./Slices/detailSlice";
