2c10a588d2b908c6145bdd7248a71292e1b69dd0
Martin Lamacchia <martinjiro22@gmail.com>
2023-10-27 15:40:14 -0300

    No deja ir al Detalle si no esta logueado

--- Client/src/components/Card/Professional.jsx
+++ Client/src/components/Card/Professional.jsx
@@ -6,8 +6,21 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 
 import { CardActionArea } from "@mui/material";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useSelector } from "react-redux";
+import Login from "../Login/Login";
 
-const Professional = ({ id, name, lastName, location, description, profession, image, categories }) => {
+const Professional = ({
+  id,
+  name,
+  lastName,
+  location,
+  description,
+  profession,
+  image,
+  categories,
+  setContainerLogin
+}) => {
   const fullName = `${name} ${lastName}`;
 
   const cardStyle = {
@@ -16,8 +29,7 @@ const Professional = ({ id, name, lastName, location, description, profession, i
     display: "flex",
     flexDirection: "column",
     height: "42em",
-    width: "300px"
-    
+    width: "300px",
   };
 
   const imageStyle = {
@@ -45,12 +57,24 @@ const Professional = ({ id, name, lastName, location, description, profession, i
     textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
   };
 
-  
+  const {isAuthenticated} = useAuth0()
+  const users = useSelector(state => state.usersLogin.user)
+
+  const handlerLogin = () => {
+    isAuthenticated || users.name ? `/detail/${id}` : setContainerLogin(true)
+  }
+
   return (
-    <Link to={`/detail/${id}`}>
-      <Card sx={cardStyle}>
+    <Link to='#' onClick={handlerLogin}>
+      <Card sx={cardStyle} >
         <CardActionArea>
-          <CardMedia component="img" height="400em" image={image} alt="prueba" sx={imageStyle} />
+          <CardMedia
+            component="img"
+            height="400em"
+            image={image}
+            alt="prueba"
+            sx={imageStyle}
+          />
           <Typography variant="h4" component="div" sx={nameStyle}>
             {fullName}
           </Typography>
@@ -58,21 +82,21 @@ const Professional = ({ id, name, lastName, location, description, profession, i
             {location}
           </Typography>
           <CardContent>
-          <Typography variant="h4" component="div">
-            {profession}
+            <Typography variant="h4" component="div">
+              {profession}
             </Typography>
             <Typography variant="h5" component="div">
-            ⭐5.0 (20 reviews)
+              ⭐5.0 (20 reviews)
             </Typography>
             <Typography variant="body2" color="text.secondary">
               {description}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Categories: {categories} 
+              Categories: {categories}
             </Typography>
           </CardContent>
         </CardActionArea>
-        </Card>
+      </Card>
     </Link>
   );
 };
