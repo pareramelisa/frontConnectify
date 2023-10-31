7106077edadb2403805e60d4271f90149d3d6a37
pareramelisa <pareralucenamelisa@gmail.com>
2023-10-31 20:38:21 -0300

    fix build error

--- Client/src/components/ViewsPayments/ViewsPayments.jsx
+++ Client/src/components/ViewsPayments/ViewsPayments.jsx
@@ -3,7 +3,7 @@ import style from './ViewsPayments.module.css';
 const VITE_API_BASE = import.meta.env.VITE_API_BASE
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import PaymentsCard from '../paymentsCard/PaymentsCard';
+import PaymentsCard from '../PaymentsCard/PaymentsCard';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
