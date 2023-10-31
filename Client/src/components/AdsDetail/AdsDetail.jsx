b94a23de45cddf70d5b679495debd95ca72ea2e0
Daniela Dell Acqua <ddacqua@gmail.com>
2023-10-31 19:49:46 -0300

    quite el boton de favoritos con notificacion

--- Client/src/components/AdsDetail/AdsDetail.jsx
+++ Client/src/components/AdsDetail/AdsDetail.jsx
@@ -28,7 +28,7 @@ import StarIcon from '@mui/icons-material/Star';
 //import NotificationsIcon from '@mui/icons-material/Notifications';
 import { useAuth0 } from '@auth0/auth0-react';
 
-import FavoritesNotification from '../FavoritesNotification/FavoritesNotifitation';
+
 import { addFavorite, removeFavorite } from '../../redux/Slices/favoritesSlice';
 
 
@@ -129,7 +129,7 @@ const DetailAd = () => {
                   </Badge> */}
 
  
-                <FavoritesNotification/>
+                
                 
                 <Link to={userData && userData.nickname && `/payments/${userData.nickname}`}>
                       <Button variant="outlined" sx={{ marginLeft: '15px' }}> Pagos </Button>
