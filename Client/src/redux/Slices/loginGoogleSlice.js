9aab2ce5b0fb22eb66aa4e9bc3c91703b32d349b
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-04 07:56:25 -0300

    Persistencia con Google

--- Client/src/redux/Slices/loginGoogleSlice.js
+++ Client/src/redux/Slices/loginGoogleSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const VITE_API_BASE = import.meta.env.VITE_API_BASE || 'localhost'
+const VITE_API_BASE = import.meta.env.VITE_API_BASE || "localhost";
 
 // Setea el estado de usuario cuando hacen login o logout
 export const loginWithGoogleSlice = createSlice({
@@ -22,8 +22,7 @@ export default loginWithGoogleSlice.reducer;
 // Hace el fetch del para el login del usuario
 export const fetchUserLoginWithGoogle = (form) => {
   return async (dispatch) => {
-    let endpoint = VITE_API_BASE + `/client/googlelogin`
-      // "https://connectifyback-dp-production.up.railway.app/client/googlelogin";
+    let endpoint = VITE_API_BASE + `/client/googlelogin`;
 
     try {
       const { data } = await axios.post(endpoint, form);
