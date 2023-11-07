bb9d3d4cd0c1310483c3c2d55a751c271e673ad4
Andy Ojeda <tuar_a@hotmail.com>
2023-11-07 20:43:25 -0300

    Reparaciones y cambios en Landing

--- Client/src/components/Landing/Landing.jsx
+++ Client/src/components/Landing/Landing.jsx
@@ -1,17 +1,26 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Typography } from "@mui/material";
-import "./Landing.css";
-import Logo from "../../assets/connectify.svg";
-import Logo2 from "../../assets/logo.svg";
+// import { Button, Typography } from "@mui/material";
+import style from './Landing.module.css';
+// import Logo from "../../assets/connectify.svg";
+// import Logo2 from "../../assets/logo.svg";
 import { useAuth0 } from "@auth0/auth0-react";
 import { AiOutlineHome } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import Login from "../Login/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { loginWithGoogle } from "../../redux/Slices/loginGoogleSlice";
-import IconButton from '@mui/material/IconButton'
-import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+// import IconButton from '@mui/material/IconButton'
+// import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
 import { fetchGetAllFavorites } from "../../redux/Slices/favoritesSlice";
+
+import Cover from '../Cover/Cover'
+
+
+import Mujer from '../../assets/Mujer.png';
+import Logo1 from '../../assets/LogoC.png';
+import Titulo from '../../assets/ConnectifyLetras.png'
+import home from '../../assets/home.png'
+
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -45,107 +54,37 @@ function LandingPage() {
   console.log(users._id);
   console.log(favorites)
 
-  return (
-    <div style={{
-      position: 'absolute',
-      width: '100%',
-    }}>
-    <div className="container">
-      {containerLogin ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100vh",
-            padding: "20px",
-            background: "rgba(0,0,0,0.5)",
-            zIndex: "10",
-          }}
-        >
-          <Login
-            setContainerLogin={setContainerLogin}
-            setPopUpLogin={setPopUpLogin}
-          />
-        </div>
-      ) : null}
-      {popUpLogin && (
-        <div
-          style={{
-            position: "absolute",
-            width: "25rem",
-            height: "10rem",
-            top: "38%",
-            left: "36%",
-            border: "2px solid black",
-            borderRadius: "20px",
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            backgroundColor: "rgba(255,255,255,0.9)",
-            zIndex: "1000",
-          }}
-        >
-          <IconButton
-            disableElevation
-            style={{
-              position: "absolute",
-              top: "5px",
-              right: "5px",
-              color: "#000000",
-              fontWeight: "bold",
-            }}
-            onClick={handlerCloseLoginPopUp}
-          >
-            <CancelRoundedIcon />
-          </IconButton>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <h3>Email y/o Password incorrectos</h3>
+   return (
+    <>
+      <Cover />
+
+      <div className={style.contenedorLanding}>
+          <div className={style.contMujer}>
+            <img className={style.imgMujer} src={Mujer} alt="" />
           </div>
-        </div>
-      )}
-      <div className="landing">
-        <div className="container-logo">
-          <img src={Logo2} alt="Logo" className="logo2" />
-          <img src={Logo} alt="Logo" className="logo" />
-        </div>
-        <Typography variant="h5" color="#545454">
-          Encuentra a los mejores profesionales para tus necesidades.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            navigate("/home");
-          }}
-          className="button"
-        >
-          <AiOutlineHome style={{ fontSize: "2em" }} />
-        </Button>
+          <div className={style.contConnectify}>
+            <div className={style.contLogo}>
+              <img className={style.imgLogo} src={Logo1} alt="" />
+            </div>
+            <div className={style.contTitulo}>
+              <img className={style.imgTitulo} src={Titulo} alt="" />
+            </div>
+            <div className={style.subTitulo}>
+              <h2>Encontrá profesionales de una manera segura.</h2>
+            </div>
+            <div className={style.contButtons}>
+              <button className={style.home} onClick={() => navigate("/home")}>
+                <img src={home} alt="" />
+                {/* HOME */}
+              </button>
+              {/* <button className={style.Login}>LOGIN</button> */}
+            </div>
+          </div>
       </div>
-      {!isAuthenticated && !users.name && (
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handlerButtonLogin}
-          className="button"
-        >
-          Ir a Login
-        </Button>
-      )}
-    </div>
-    </div>
+	
+	
+
+    </>
   );
 }
 
