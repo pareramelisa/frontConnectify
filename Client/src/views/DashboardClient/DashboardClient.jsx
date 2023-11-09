0d0d729beca8c16ffa34c2c7bd4d1d8d5d7a5984
Martin Lamacchia <martinjiro22@gmail.com>
2023-11-09 20:14:14 -0300

    Arreglando bug Nav2

--- Client/src/views/DashboardClient/DashboardClient.jsx
+++ Client/src/views/DashboardClient/DashboardClient.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { Grid } from '@mui/material';
 import UserInfoCard from './UserInfoCardClient';
 import ButtonBack from '../../components/Utils/ButtonBack/ButtonBack';
-import NavBarDemo2 from '../../components/NavBarDemo2/NavBarDemo2';
+import Navbar from '../../components/Navbar/Navbar'
 import ReviewItem from '../../components/ReusableComponents/ReviewShow';
 //import { useHistory } from 'react-router-dom';
 
@@ -47,7 +47,7 @@ const DashboardClient = () => {
 
   return (
     <div>
-      <NavBarDemo2/>
+      <Navbar/>
       <div style={{ margin: '0em 3em' }}>
     <ButtonBack /> 
     
