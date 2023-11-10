3ef8938f752402fa00d6d111346a837a987b1328
pareramelisa <pareralucenamelisa@gmail.com>
2023-11-10 19:12:15 -0300

    fix ad id

--- Client/src/components/AdsProfessional/AdsProfessional.jsx
+++ Client/src/components/AdsProfessional/AdsProfessional.jsx
@@ -65,7 +65,7 @@ function AdsProfesional() {
       </ListSubheader>
       {adsFilter.map((ad) => (
         <ListItem
-          key={ad._id}
+          key={ad.id}
           sx={{ padding: "15px"}}
           disableGutters
           secondaryAction={
